7798823a9221034590b3134bad000b146b332b2c
Milton Guillermo Rivas Palacios <00019725@uca.edu.sv>
2025-04-28 15:05:17 -0600

    Add files via upload

---
+++ ejercicio.cpp
@@ -0,0 +1,38 @@
+#include <iostream> //libreria
+using namespace std;
+
+int main() //funcion 
+{
+    cout << "Estas en la lista de Kraven" << endl;
+    cout << "ingrese que cosa eres" << endl;
+    
+
+    string cosa = "persona"; //declara variable con el personaje que eres
+    string animal = "animal"; //declara variable con el personaje que eres
+    string malo = "malo"; //declara variable con el personaje que eres
+
+    string personaje = ""; // pregunta el tipo de sujeto que eres como persona, animal o malo
+
+    cin >> personaje;
+
+
+    if (personaje == cosa) //compara si ingresaste la palabra del tipo de personaje que eres 
+    {
+        cout << "Te saluda" << endl;
+        
+    }
+    else if (personaje == animal)//compara si ingresaste la palabra del tipo de personaje que eres
+    {
+        cout << "Eres una presa" << endl;
+    }
+    else if (personaje == malo)//compara si ingresaste la palabra del tipo de personaje que eres
+    {
+        cout << "Te salvastes no eres una presa de kraven" << endl;
+    }
+    else 
+    {
+        ; 
+    }
+
+   return 0;
+}
